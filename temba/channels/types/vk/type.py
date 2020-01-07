7dcfb433b2b3f7343a235baadeb07b6ea54ed4bd
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-01-07 10:24:48 -0300

    Define atribbutes for VK Channel

--- temba/channels/types/vk/type.py
+++ temba/channels/types/vk/type.py
@@ -12,6 +12,22 @@ class VKType(ChannelType):
     """
     A VK channel
     """
+
+    code = "VK"
+    category = ChannelType.Category.SOCIAL_MEDIA
+
+    courier_url = r"^vk/(?P<uuid>[a-z0-9\-]+)/receive"
+
+    name = "VK"
+    icon = "icon-vk-official"
+
+    claim_blurb = _(
+        """Add a <a href="http://vk.com">VK</a> bot to send and receive messages on behalf
+    of one of your VK pages for free. You will need to create a VK application on their
+    <a href="https://vk.com/dev">developers</a> site first."""
+    )
+    claim_view = ClaimView
+    
     max_length = 320
     attachment_support = True
     free_sending = True
